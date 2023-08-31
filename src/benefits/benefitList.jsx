087645825050efb8edcb792a6abce146c30d0e1a
FrotaLucas <lucasdiasfrota@gmail.com>
2023-08-31 15:26:05 +0200

    create file jsx with list of itens

---
+++ src/benefits/benefitList.jsx
@@ -0,0 +1,22 @@
+const jsonData = [
+  {
+    title: 1,
+    content: 'Competitive Rates',
+  },
+  {
+    title: 1,
+    content: 'Production Optimization',
+  },
+  {
+    title: 2,
+    content: 'Quick release of funds in the account',
+  },
+  {
+    title: 3,
+    content: 'Soybeans and corn - soon other crops',
+  },
+];
+
+export default jsonData;
+
+//tentar fazer depois em arquivo formato json
