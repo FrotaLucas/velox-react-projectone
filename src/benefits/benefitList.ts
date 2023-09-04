b9e9192ca4ef9a333496bdf49f95589b594b31c6
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-04 16:16:46 +0200

    edit array of benefits

--- src/benefits/benefitList.ts
+++ src/benefits/benefitList.ts
@@ -1,22 +1,26 @@
-const jsonData = [
+const benefitList = [
   {
-    title: 1,
-    content: 'Competitive Rates',
+    title: 'Competitive Rates',
+    subtitle: 'Competitive rates',
+    content: 'With the support of the Velox team, rural producers will have access to advantageous rates, especially compared to those practiced in operations among participants in the agricultural sector itself.',
   },
   {
-    title: 2,
-    content: 'Production Optimization',
+    title: 'Production Optimization',
+    subtitle: 'Production Optimization',
+    content: 'Focus on your main core activity, and we guarantee the best internet that will boost your production.',
   },
   {
-    title: 3,
-    content: 'Quick release of funds in the account',
+    title:  'Quick release of fast internet connection',
+    subtitle: 'Quick release of funds in the account',
+    content: 'Count on a much more agile internet granting process compared to other market players.',
   },
   {
-    title: 4,
-    content: 'Soybeans and corn - soon other crops',
+    title: 'Soybeans and corn',
+    subtitle: 'Soybeans and corn - soon other crops',
+    content: 'No matter which agricultural property you are on, our services will reach you with the best quality.',
   },
 ];
 
-export default jsonData;
+export {benefitList};
 
 //tentar fazer depois em arquivo formato json
