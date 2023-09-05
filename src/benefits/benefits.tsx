4d7333c6965b0bfc830b67a9b65e63456ab66913
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-05 16:15:27 +0200

    set responsiviness with useEffect

--- src/benefits/benefits.tsx
+++ src/benefits/benefits.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { SectionContainer, ListBenefitis, DivContainer, HeaderContent, TitleContent,SubtitleContent, BodyContent, LineContent,SquareContent } from './style'
+import { SectionContainer, ListBenefitis, DivContainer, HeaderContent, TitleContent,SubtitleContent, BodyContent, LineContent,SquareContent, BenefitsButton } from './style'
 import {benefitList} from './benefitList';
+
 
 interface BenefitsInterface {
   title: string;
@@ -9,12 +10,23 @@ interface BenefitsInterface {
 }
 
 const Benefits: React.FC = ()=> {
-  
 const [state, useState] = React.useState<BenefitsInterface | null>(benefitList[0])
 function handleClick(index){
   let selected = benefitList[index];
   useState(selected);
 }
+
+const [isMObile, setIsMobile] = React.useState(window.innerWidth <= 1024)
+React.useEffect( ()=> {
+  const handleResize = () => { setIsMobile(window.innerWidth <=1024)}
+  window.addEventListener('resize', handleResize)
+
+  //unmounts of the component
+  return ( () => window.removeEventListener('resize', handleResize))
+
+}, [])
+
+
   return <SectionContainer>
           <DivContainer>
             <HeaderContent>
@@ -44,7 +56,9 @@ function handleClick(index){
                 <h2>{state?.subtitle}</h2>
                 <p>{state?.content}</p>
               </SquareContent>
+              
             </BodyContent>
+               {isMObile && <BenefitsButton>Request contact</BenefitsButton>}
 
           </DivContainer>
         </SectionContainer>
