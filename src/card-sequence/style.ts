24e384c4675a07fab8dd44327fbb6a03254e40bd
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-06 16:23:55 +0200

    create components title and subtile

---
+++ src/card-sequence/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const SectionContainer = styled.section`
+  width: 100vw;
+  @media (max-width: 1024px) {
+  }
+`;
+export { SectionContainer };
+
+const DivContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+  }
+`;
+export { DivContainer };
+const TitleContent = styled.h2`
+   margin-bottom: 2rem
+`;
+export { TitleContent };
+
+const SubtitleContent = styled.p`
+  margin-bottom: 2rem
+`;
+export { SubtitleContent };
