32c6bd2be1ac9a554e102b5038e0ef90f7024133
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-06 16:23:40 +0200

    implement main components

---
+++ src/card-sequence/cards.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { TitleContent, SubtitleContent, SectionContainer, DivContainer } from "./style";
+const Cards: React.FC = () => {
+
+  return <SectionContainer>
+    <DivContainer>
+      <TitleContent>Service without bureaucracy!</TitleContent>
+      <SubtitleContent>Request and track the process in a few steps through the platform.</SubtitleContent>
+    </DivContainer>
+  </SectionContainer>
+}
+
+export default Cards;
