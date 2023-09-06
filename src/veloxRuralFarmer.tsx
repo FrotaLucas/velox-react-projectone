a5e75ad1136452e00e79a055eb612cf5d78f2b21
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-06 16:23:19 +0200

    import Card component

--- src/veloxRuralFarmer.tsx
+++ src/veloxRuralFarmer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './header-with-banner/header';
+import Cards from './card-sequence/cards';
 import Benefits from './benefits/benefits';
+
 
 const VeloxRuralFarmer = () => {
   return (
     <>
       <Header/>
+      <Cards/>
       <Benefits/>
     </>
   );
