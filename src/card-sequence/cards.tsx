791aaed895491ef1d02bcf63262a39a12aab2a5a
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-06 17:12:39 +0200

    implement Card Structure

--- src/card-sequence/cards.tsx
+++ src/card-sequence/cards.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { TitleContent, SubtitleContent, SectionContainer, DivContainer } from "./style";
+import { TitleContent, SubtitleContent, SectionContainer, DivContainer, CardRow, Card, CardHead } from "./style";
+import { cardContent } from "./contentCard";
+
 const Cards: React.FC = () => {
 
-  return <SectionContainer>
+  return (
+    <SectionContainer>
     <DivContainer>
       <TitleContent>Service without bureaucracy!</TitleContent>
       <SubtitleContent>Request and track the process in a few steps through the platform.</SubtitleContent>
+      <CardRow>
+        {cardContent.map((item, index)=> 
+        <Card key={index}>
+           <CardHead> 
+             <img src={`./public/svgs/${item.svg}.svg`}/>
+             <h3>{item.title}</h3>
+           </CardHead>
+           <p>{item.content}</p>
+        </Card>)}
+      </CardRow>
+
     </DivContainer>
   </SectionContainer>
+
+  )
+
 }
 
 export default Cards;
