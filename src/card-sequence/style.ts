c7e216fb6c2dfb98514c9d7b320e69accda32045
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-07 14:17:15 +0200

    adjust margin of Title and Subtitle and create button

--- src/card-sequence/style.ts
+++ src/card-sequence/style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const SectionContainer = styled.section`
   width: 100vw;
   background-color: rgb(230, 231, 234);
+  
   @media (max-width: 1024px) {
   }
 `;
@@ -15,14 +16,20 @@ const DivContainer = styled.div`
   }
 `;
 export { DivContainer };
+
 const TitleContent = styled.h2`
-   margin-bottom: 2rem
+  margin: unset;
+  padding-bottom: 2rem;
+  padding-top: 5rem;
 `;
 export { TitleContent };
 
 const SubtitleContent = styled.p`
-  margin-bottom: 2rem
+  margin: unset;
+  padding-bottom: 2rem;
+
 `;
+
 export { SubtitleContent };
 
 const CardRow = styled.div`
@@ -43,3 +50,26 @@ const CardHead = styled.div`
     //align-items: center;
 `
 export { CardHead}
+
+
+const CardButton = styled.button`
+    margin: 2rem 0;
+    font-family: Moderat,sans-serif;
+    background-color: #195ab4;
+    color: #fff;
+    font-weight: 400;
+    font-size: 15px;
+    padding: 10px;
+    line-height: 16px;
+    cursor: pointer;
+    border: none;
+    border-color: transparent;
+    border-style: solid;
+    border-radius: 4px;
+    outline: none;
+    align-items: center;
+    justify-content: center;
+    transition-property: all;
+    transition-duration: .15s;
+`
+export {CardButton};
