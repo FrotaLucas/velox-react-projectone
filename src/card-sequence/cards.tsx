c6c78b9d49523b4cd4691c07e23dd6813d48756e
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-11 13:03:15 +0200

    add classes of carousel to manipulate

--- src/card-sequence/cards.tsx
+++ src/card-sequence/cards.tsx
@@ -1,15 +1,42 @@
 import React from "react";
+import Slider from "react-slick"
 import { TitleContent, SubtitleContent, SectionContainer, DivContainer, CardRow, Card, CardHead, CardButton } from "./style";
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import { cardContent } from "./contentCard";
 
+interface SliderSettings {
+  dots: boolean;
+  infinite: boolean;
+  speed: number;
+  //rows: number;
+  slidesToShow: number;
+  slidesToScroll: number;
+  autoplay: boolean;
+  autoplaySpeed: number;
+};
+
 const Cards: React.FC = () => {
+  const settings: SliderSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    //rows: 1,
+    slidesToShow: 1, // Number of slides to show at once
+    slidesToScroll: 1, // Number of slides to scroll when navigating
+    autoplay: false, // Enable autoplay
+    autoplaySpeed: 3000, // Autoplay speed in milliseconds
+  };
+
 
   return (
     <SectionContainer>
     <DivContainer>
         <TitleContent>Service without bureaucracy!</TitleContent>
         <SubtitleContent>Request and track the process in a few steps through the platform.</SubtitleContent>
-      <CardRow>
+      {/* <CardRow>
         {cardContent.map((item, index)=> 
         <Card key={index}>
            <CardHead> 
@@ -18,7 +45,26 @@ const Cards: React.FC = () => {
            </CardHead>
            <p>{item.content}</p>
         </Card>)}
+      </CardRow> */}
+      <CardRow>
+        <Slider {...settings}>
+          { cardContent.map((item,index)=> 
+            <Card key={index}>
+              <CardHead>
+               <img src={`./public/svgs/${item.svg}.svg`}/>
+               <h3>{item.title}</h3>
+              </CardHead>
+              <p>
+                {item.content}
+              </p>
+            </Card>)}
+
+        </Slider>
       </CardRow>
+    
+
+  
+      
       <CardButton> Fill form</CardButton>
     </DivContainer>
   </SectionContainer>
