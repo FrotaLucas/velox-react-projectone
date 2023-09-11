c74dd717b5b5dc5e220e10919aa01e04b92e5be8
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-11 13:04:02 +0200

    add properties to Card and Cardhead

--- src/card-sequence/style.ts
+++ src/card-sequence/style.ts
@@ -35,19 +35,50 @@ export { SubtitleContent };
 const CardRow = styled.div`
   display: flex;
   gap: 1rem;
+
+  & .slick-list {
+    max-width: 80vw;
+  }
+
+
+  @media (max-width: 1024px) {
+    display: unset;
+    gap: unset;
+  
+  & .slick-slide > div {
+    margin-left: 1rem; /* Add margin to each slide's content */
+    margin-right: 1rem;
+    //background-color: #f0f0f0; /* Background color for each slide */
+    //border: 1px solid #ddd; /* Border for each slide */
+    //padding: 2rem; /* Padding for each slide's content */
+    text-align: center; /* Center-align text within each slide */
+    //box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2); /* Box shadow for each slide */
+}
+  & .slick-slide > div > div {
+    width: 80% !important;
+    height: 10rem;
+    padding: 10px;
+  }
+ 
+  }
 `;
 export {CardRow}
 const Card = styled.div`
     background-color: white;
-    padding: 24px;
-    width: 200px;
-    line-height: 150%;
+    //height: 5rem;
+    //padding: 10px;
+    //width: 200px;
 `
 export {Card};
 
-const CardHead = styled.div`
+const CardHead = styled.span`
     display: flex;
     //align-items: center;
+    //align-items: center;
+    @media (max-width: 1024px){
+      gap: 0.5rem;
+      align-items: flex-end;
+    }
 `
 export { CardHead}
 
