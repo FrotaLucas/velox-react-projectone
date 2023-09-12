b201847221f4d0550b084e6ebb1f56fdf0053c12
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 13:23:05 +0200

    create component Visibility

---
+++ src/visibility/visibility.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionContainer, { DivContainer } from './style'
+
+const Visibility: React.FC = () => {
+  return <SectionContainer>
+    <DivContainer>
+    ha
+    </DivContainer>
+  </SectionContainer>
+}
+
+export default Visibility
