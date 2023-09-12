acc9b7065e26e1a407847e8ce3fae0d591e39cb9
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 14:14:29 +0200

    implement properties of DivContainer

--- src/visibility/style.ts
+++ src/visibility/style.ts
@@ -13,17 +13,26 @@ const SectionContainer = styled.section`
 const DivContainer = styled.div`
   width: 80%;
   margin: 0 auto;
-  padding: 5rem 0;
+ 
+`;
+export  {DivContainer};
+
+const DivContent = styled.div`
+  padding-top: 5rem;
   display: flex;
   gap: 10%;
   @media (max-width: 1024px) {
     flex-direction: column;
+    padding-top: 0;
   }
-`;
-export  {DivContainer};
+`
+export {DivContent}
 
 const InnerText = styled.div`
   width: 50%;
+  @media (max-width: 1024px){
+    width: 80%;
+  }
 `
 export {InnerText};
 
@@ -40,3 +49,26 @@ const SubtitleContent = styled.p`
 `;
 
 export { SubtitleContent };
+
+const VisibilityButton = styled.button`
+    margin: 2rem 0;
+    width: 10rem;
+    font-family: Moderat,sans-serif;
+    background-color: #195ab4;
+    color: #fff;
+    font-weight: 400;
+    font-size: 15px;
+    padding: 10px;
+    line-height: 16px;
+    cursor: pointer;
+    border: none;
+    border-color: transparent;
+    border-style: solid;
+    border-radius: 4px;
+    outline: none;
+    align-items: center;
+    justify-content: center;
+    transition-property: all;
+    transition-duration: .15s;
+`
+export {VisibilityButton};
