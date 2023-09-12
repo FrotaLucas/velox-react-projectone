8acbd9c9fd43d6a3dfb1bc7fa43891672a9e51d9
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 13:23:25 +0200

    create Section and Div

---
+++ src/visibility/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+
+const SectionContainer = styled.section`
+  width: 100vw;
+  background-color: rgb(230, 231, 234);
+  
+  @media (max-width: 1024px) {
+  }`
+
+  export default SectionContainer
+
+const DivContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+  }
+`;
+export { DivContainer };
