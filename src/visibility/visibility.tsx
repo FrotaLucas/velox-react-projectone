bd093b56b243b0376aefaf5c23c7ef004af6bb50
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 13:54:40 +0200

    Implement new componets

--- src/visibility/visibility.tsx
+++ src/visibility/visibility.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import{ SectionContainer,  DivContainer, InnerContainer, TitleContent, SubtitleContent}  from './style'
+import{ SectionContainer,  DivContainer, InnerText, TitleContent, SubtitleContent}  from './style'
 
 const Visibility: React.FC = () => {
   return (
   <SectionContainer>
     <DivContainer>
-      <InnerContainer>
+      <InnerText>
         <TitleContent> More visibility</TitleContent>
         <SubtitleContent> Access all steps of internet granting on our exclusive platform. The system is simple and intuitive and provides a quick journey without the bureaucracy compared with other players.
         </SubtitleContent>          
-
-      </InnerContainer>
+      </InnerText>
+      <img src='./public/images/credito-agro-analysis.png'/>
     </DivContainer>
   </SectionContainer>)
 }
