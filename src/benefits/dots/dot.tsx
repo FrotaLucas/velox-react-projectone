e0812848b18adbdb0fc25b05c404be1968747d35
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 15:54:22 +0200

    implement component of Dot

--- src/benefits/dots/dot.tsx
+++ src/benefits/dots/dot.tsx
@@ -1,1 +1,17 @@
 import React from 'react'
+import { Item } from './style'
+
+interface DotProps {
+  backgroundColor: boolean;
+}
+
+const Dot: React.FC<DotProps> = ({backgroundColor}) => {
+
+  return (
+    <Item toggle={backgroundColor}>
+      Oiee
+    </Item>
+
+  )
+}
+export default Dot;
