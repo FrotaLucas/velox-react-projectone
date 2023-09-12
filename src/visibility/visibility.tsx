2c7930bdada5adf32d4824631e29f87ca54fcc87
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-12 13:36:06 +0200

    implement Innercontainer, TItle Container, Subtitle

--- src/visibility/visibility.tsx
+++ src/visibility/visibility.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import SectionContainer, { DivContainer } from './style'
+import{ SectionContainer,  DivContainer, InnerContainer, TitleContent, SubtitleContent}  from './style'
 
 const Visibility: React.FC = () => {
-  return <SectionContainer>
+  return (
+  <SectionContainer>
     <DivContainer>
-    ha
+      <InnerContainer>
+        <TitleContent> More visibility</TitleContent>
+        <SubtitleContent> Access all steps of internet granting on our exclusive platform. The system is simple and intuitive and provides a quick journey without the bureaucracy compared with other players.
+        </SubtitleContent>          
+
+      </InnerContainer>
     </DivContainer>
-  </SectionContainer>
+  </SectionContainer>)
 }
 
 export default Visibility
