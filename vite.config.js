bd3aab256eaf2e4387733c7ccc5accf80ee47a19
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-21 12:57:49 +0200

    create a new folder build to host the project

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This specifies the output directory for the production build.
+    outDir: 'build', // This specifies the output directory for the production build.
   },
 });
