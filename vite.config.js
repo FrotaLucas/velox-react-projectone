3cc5b4dfdbbc1398af5ffe63797b8de1a3c95799
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-21 15:02:17 +0200

    adjust file vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: '/velox-react-projectone',
-    outDir: 'build', // This specifies the output directory for the production build.
-  },
+  base: '/velox-react-projectone',
 });
